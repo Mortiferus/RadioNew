9a71c8a9d54f331252595130b2fefa86fd88c5c5
Nikita Reshetnikov <Mrnikinik@yahoo.com>
2020-12-26 11:15:52 +0300

    new branch

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,72 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int station = 0;
+    private int volume = 3;
+
+    /* Установка параметров */
+
+    public void setStation(int station) {
+        if (station > 9) {
+            return;
+        }
+        if (station < 0) {
+            return;
+        }
+        this.station = station;
+    }
+
+    public void setVolume(int volume) {
+        if (volume > 10) {
+            return;
+        }
+        if (volume < 0) {
+            return;
+        }
+        this.volume = volume;
+    }
+
+    /* Оперирование станциями */
+
+    public void switchStationUp() {
+        if (station == 9) {
+            setStation(0);
+            return;
+        }
+        setStation(station + 1);
+    }
+
+    public void switchStationDown() {
+        if (station == 0) {
+            setStation(9);
+            return;
+        }
+        setStation(station - 1);
+    }
+
+    /* Регулировка громкости */
+
+    public void increaseVolume() {
+        if (volume == 10) {
+            return;
+        }
+        setVolume(volume + 1);
+    }
+
+    public void decreaseVolume() {
+        if (volume == 0) {
+            return;
+        }
+        setVolume(volume - 1);
+    }
+
+    /* Getters */
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int getStation() {
+        return station;
+    }
+}
